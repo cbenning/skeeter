d636e809bc052118a68d6e2364d52f493391757a
Chris Benninger <chrisbenninger@gmail.com>
2012-11-17 17:58:11 -0800

    asd

--- src/main/scala/ca/benninger/skeeter/ScraperActor.scala
+++ src/main/scala/ca/benninger/skeeter/ScraperActor.scala
@@ -33,67 +33,81 @@ class ScraperActor extends Actor{
     
     def receive = {
     	
-        case URLGetResponse(url) => 
-
-            try{
-                val doc = Jsoup.connect(url).timeout(0).get
-
-                //Scrape links
-                val links = doc.select("a")
-                var tolook = Map[String,Boolean]()
-                val domain = "http://www.mec.ca"
-                val regex = new Regex(domain+"/AST/")
-
-                regex findFirstIn url match{
-                    case Some(regex) =>  
-                        tolook += new_url -> false
-                    case None => 0
-                }
-
-                for ( link <- links) {
-                    val new_url = domain+link.attr("href")
-                    regex findFirstIn new_url match{
-                        case Some(regex) =>  
-                            tolook += new_url -> false
-                        case None => 0
-                    }
-                }
-
-
-                if(tolook.size>0){
-                    this.urllist ! new NewURLList(tolook)
-                }
-
-
-                //Scrape data
-                val madeIn = doc.select("span.prDs").text
-                if(madeIn.contains("Made in Canada")) {
-                    val root = doc.select("div#shopbox")
-                    val name = root.select("h1").text
-                    val price = root.select("div#idPrdPrice span.prPr").text
-                    val reviewLink = root.select("div.merch-rating a:eq(1)").attr("href")
-                    val image = root.select("div#skuColours a.cloud-zoom-gallery").attr("href")
-                    println("Found one!: "+name)
-                }
-                // else {
-                //     if(madeIn != ""){
-                //         println("Product was : "+madeIn)
-                //     }
-                //     else{
-                //         println("Not a product page")   
-                //     }
-                // }
+        case URLGetResponse(url) =>
+            val regex = new Regex("(http://www.mec.ca|/AST)")
+            regex findFirstIn url match{
+                case Some(regex) =>
+                    println("0")
+                    processURL(url)
+                case None => 0
             }
-            catch{
-                case e => println(e)
-            }
-
             this.urllist ! new URLGetRequest
             context.setReceiveTimeout(3 seconds)
 
         case ReceiveTimeout =>
             this.urllist ! new URLGetRequest
             context.setReceiveTimeout(3 seconds)
+    }
+
+    def processURL(url:String) = {
+
+        try{
+            val doc = Jsoup.connect(url).timeout(0).get
+
+            //Scrape links
+            val links = doc.select("a")
+            var tolook = Map[String,Boolean]()
+
+            //
+            // regex findFirstIn url match{
+            //     case Some(regex) =>  
+            //         tolook += new_url -> false
+            //     case None => 0
+            // }
+
+            //Search for valid urls
+            for ( link <- links) {
+
+                val regex_http = new Regex("http://")
+                regex_http findFirstIn url match{
+
+                    case Some(regex_http) =>  
+                        tolook += url -> false
+
+                    case None =>
+                        val new_url = "http://www.mec.ca"+link.attr("href")
+                        tolook += new_url -> false
+
+                }
+            }
+
+            //If we found urls, submit them
+            if(tolook.size>0){
+                this.urllist ! new NewURLList(tolook)
+            }
+
+            //Scrape data
+            val madeIn = doc.select("span.prDs").text
+            if(madeIn.contains("Made in Canada")) {
+                val root = doc.select("div#shopbox")
+                val name = root.select("h1").text
+                val price = root.select("div#idPrdPrice span.prPr").text
+                val reviewLink = root.select("div.merch-rating a:eq(1)").attr("href")
+                val image = root.select("div#skuColours a.cloud-zoom-gallery").attr("href")
+                println("Found one!: "+name)
+            }
+            // else {
+            //     if(madeIn != ""){
+            //         println("Product was : "+madeIn)
+            //     }
+            //     else{
+            //         println("Not a product page")   
+            //     }
+            // }
+        }
+        catch{
+            case e => println(e)
+        }
     }
 
 }
